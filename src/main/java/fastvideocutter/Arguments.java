902d088c2dd9df6421b0d8b5cb9fd8f23dbd57bd
Jami Lehtinen <jami.lehtinen1@gmail.com>
2024-10-26 01:35:53 +0300

    Added flags, improved help page, cleaned code.

---
+++ src/main/java/fastvideocutter/Arguments.java
@@ -0,0 +1,56 @@
+package fastvideocutter;
+
+public class Arguments {
+	public boolean accurateMode = false;
+	public String inputFile;
+	public String outputFilename;
+	public String startTime;
+	public String endTime;
+	
+	public static Arguments parseArguments(String[] args) {
+		Arguments arguments = new Arguments();
+		
+		boolean encounteredAFlag = false;
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+			
+			if (arg.startsWith("-")) {
+				encounteredAFlag = true;
+				
+				if (arg.equals("-i") || arg.equals("--input")) {
+					arg = args[++i];
+					arguments.inputFile = arg;
+				} else if (arg.equals("-s") || arg.equals("--start")) {
+					arg = args[++i];
+					arguments.startTime = arg;
+				} else if (arg.equals("-e") || arg.equals("--end")) {
+					arg = args[++i];
+					arguments.endTime = arg;
+				} else if (arg.equals("-d") || arg.equals("--duration")) {
+					arg = args[++i];
+					arguments.endTime = "d" + arg;
+				} else if (arg.equals("-o") || arg.equals("--output")) {
+					arg = args[++i];
+					arguments.outputFilename = arg;
+				} else if (arg.equals("-a") || arg.equals("--accurate")) {
+					arguments.accurateMode = true;
+				}
+				
+			} else if (!encounteredAFlag) {
+				if (i == 0) {
+					arguments.inputFile = arg;
+				} else if (i == 1) {
+					arguments.startTime = arg;
+				} else if (i == 2) {
+					arguments.endTime = arg;
+				} else if (i == 3) {
+					arguments.outputFilename = arg;
+				}
+			} else if (i == args.length - 1) {
+				arguments.outputFilename = arg;
+			}
+		}
+		
+		return arguments;
+	}
+}
--- src/main/java/fastvideocutter/Main.java
+++ src/main/java/fastvideocutter/Main.java
@@ -15,72 +15,91 @@ import processes.StreamGobbler;
 import processes.StreamGobbler.Type;
 import timer.DurationFormat;
 
+/*TODO: Might want to implement faster download for m3u8 files, like in TwitchRecover, see TwitchRecover.Core.Downloader.Download.TSDownload()
+It downloads them concurrently and then combines (literally just merges bytes back to back). But since this can set the times we wouldn't need to download all the .ts files.
+Not sure how to get the correct video length and start time then. Maybe if the downloaded length is long, download all and then combine and cut with ffmpeg,
+but otherwise let ffmpeg do it all the way. Or figure out a way to do it better.*/
 public class Main {
 	private static Process PROCESS = null;
 	
 	public static void main(String[] args) {
-		boolean startedWithoutArgs = false;
+		boolean askForExitWhenDone = false;
+		
+		Arguments arguments;
+		
 		if (args.length == 0) {
-			startedWithoutArgs = true;
+			arguments = new Arguments();
+			askForExitWhenDone = true;
 			
-			printHelp();
+			printShortHelp();
 			
 			Scanner scan = new Scanner(System.in);
-			System.out.print("Input file (drag and drop works, URLs work. 'm' for merge mode): ");
-			args = new String[4];
-			args[0] = scan.nextLine();
-			args[0] = args[0].replaceAll("\"", "");
+			System.out.print("Input file (drag and drop works, URLs work. 'm' for merge mode, 'a' for accurate timings (slow)): ");
+			String arg = scan.nextLine();
 			
-			if (args[0].toLowerCase().equals("m") || args[0].toLowerCase().equals("merge")) {
+			if (arg.toLowerCase().equals("h") || arg.toLowerCase().equals("help")) {
+				printFullHelp();
+				waitForEnter();
+				return;
+			} else if (arg.toLowerCase().equals("m") || arg.toLowerCase().equals("merge")) {
 				merge();
 				return;
+			} else if (arg.toLowerCase().equals("a") || arg.toLowerCase().equals("accurate")) {
+				arguments.accurateMode = true;
+				System.out.print("Input file (drag and drop works, URLs work): ");
+				arguments.inputFile = scan.nextLine();
+				arguments.inputFile = arguments.inputFile.replaceAll("\"", "");
+			} else {
+				arguments.inputFile = arg.replaceAll("\"", "");
 			}
 			
 			System.out.print("Start time: ");
-			args[1] = scan.nextLine();
+			arguments.startTime = scan.nextLine();
 			
-			if (args[1].isBlank()) {
-				args[1] = "0";
+			if (arguments.startTime.isBlank()) {
+				arguments.startTime = "0";
 			}
 			
 			System.out.print("End time: ");
-			args[2] = scan.nextLine();
+			arguments.endTime = scan.nextLine();
 			
 			System.out.println("New name (default is \"out\"): ");
 			System.out.println("(For absolute path put drive letter and slash first, and end it with file ext)");
-			if (isURL(args[0])) {
+			if (isURL(arguments.inputFile)) {
 				System.out.println("(One empty enter adds downloads folder automatically as a path)");
 			}
-			args[3] = scan.nextLine();
+			arguments.outputFilename = scan.nextLine();
 			
-			if (args[3].isEmpty() && isURL(args[0])) {
+			if (arguments.outputFilename.isEmpty() && isURL(arguments.inputFile)) {
 				System.out.println("Output filename (include extension):");
-				args[3] = getDownloadsFolder().getAbsolutePath() + "\\";
-				System.out.print(args[3]);
-				args[3] += scan.nextLine();
+				arguments.outputFilename = getDownloadsFolder().getAbsolutePath() + "\\";
+				System.out.print(arguments.outputFilename);
+				arguments.outputFilename += scan.nextLine();
 			}
 			
 		} else if (args.length < 2 || args[0].equals("-h")) {
-			printHelp();
+			printFullHelp();
 			return;
+		} else {
+			arguments = Arguments.parseArguments(args);
 		}
 		
 		
 		
 		String startString = "";
-		Duration start = DurationFormat.parseSimple(args[1]);
+		Duration start = DurationFormat.parseSimple(arguments.startTime);
 		if (start.toMillis() != 0) {
 			startString = " -ss " + (start.toMillis() / 1000.0);
 		}
 		
 		String endString = "";
 		
-		if (args.length > 2 && !args[2].isBlank()) {
-			if (args[2].startsWith("d")) {
-				Duration e = DurationFormat.parseSimple(args[2].substring(1));
+		if (!arguments.endTime.isBlank()) {
+			if (arguments.endTime.startsWith("d")) {
+				Duration e = DurationFormat.parseSimple(arguments.endTime.substring(1));
 				endString = " -t " + (e.toMillis() / 1000.0);
 			} else {
-				Duration e = DurationFormat.parseSimple(args[2]);
+				Duration e = DurationFormat.parseSimple(arguments.endTime);
 				endString = " -t " + (e.minus(start).toMillis() / 1000.0);
 			}
 		}
@@ -88,12 +107,12 @@ public class Main {
 		String outPath = "";
 		String outName = "out";
 		
-		String input = args[0];
+		String input = arguments.inputFile;
 		String type = input.substring(input.lastIndexOf(".") + 1);
 		
 		//If file is m3u8, it might be master file with many qualities, we don't want to download all of them.
 		//FFmpeg will choose the highest quality by default.
-		String mapStreams = "-map 0:v -map 0:a ";
+		String mapStreams = "-map 0:v -map 0:a "; //This picks all streams
 		if (type.equals("m3u8")) {
 			mapStreams = "";
 		}
@@ -103,8 +122,8 @@ public class Main {
 			outPath = input.substring(0, idx + 1);
 		}
 		
-		if (args.length == 4 && !args[3].isBlank()) {
-			outName = args[3];
+		if (!arguments.outputFilename.isBlank()) {
+			outName = arguments.outputFilename;
 			if (outName.endsWith(".mp4") || outName.endsWith(".mkv")) {
 				type = outName.substring(outName.lastIndexOf(".") + 1);
 				outName = outName.substring(0, outName.lastIndexOf("."));
@@ -116,35 +135,63 @@ public class Main {
 			startDot = "";
 		}
 		
+		String codec = " -c copy ";
+		if (arguments.accurateMode) {
+			codec = " -c:v libx264 -crf 18 -c:a aac ";
+		}
+		
 		String command;
 		if (isWindowsAbsolutePath(outName)) { //Doesn't include the extension
-			command = startDot + "ffmpeg -protocol_whitelist file,http,https,tcp,tls" + startString + " -i \"" + input + "\"" + endString + " -c copy " + mapStreams + "\"" + outName + "." + type + "\"";
+			command = startDot + "ffmpeg -protocol_whitelist file,http,https,tcp,tls" + startString + " -i \"" + input + "\"" + endString + codec + mapStreams + "\"" + outName + "." + type + "\"";
 		} else {
-			command = startDot + "ffmpeg -protocol_whitelist file,http,https,tcp,tls" + startString + " -i \"" + input + "\"" + endString + " -c copy " + mapStreams + "\"" + outPath + outName + "." + type + "\"";
+			command = startDot + "ffmpeg -protocol_whitelist file,http,https,tcp,tls" + startString + " -i \"" + input + "\"" + endString + codec + mapStreams + "\"" + outPath + outName + "." + type + "\"";
 		}
 		
 		executeCommand(command);
 		
-		if (startedWithoutArgs) {
+		if (askForExitWhenDone) {
 			System.out.println("\nDone.");
-			System.out.println("Press enter to exit.");
-			new Scanner(System.in).nextLine(); //Keeps the console open until user presses enter.
+			waitForEnter();
 		}
 	}
 	
-	private static void printHelp() {
-		System.out.println("Command format:");
-		System.out.println("FastVideoCutter.exe \"path to the mp4 file.mp4\" startTime(ss / mm:ss / hh:mm:ss.lll etc) endTime outputName\n");
+	private static void waitForEnter() {
+		System.out.println("Press enter to exit.");
+		new Scanner(System.in).nextLine(); //Keeps the console open until user presses enter.
+	}
+	
+	private static void printShortHelp() {
+		System.out.println("Type 'h' or run the exe with '-h' flag for help.\n");
+	}
+	
+	private static void printFullHelp() {
+		System.out.println("\nCommand format:");
+		System.out.println("FastVideoCutter.exe \"path to the video file.mp4\" startTime endTime outputName\n");
 		
-		System.out.println("Like this: FastVideoCutter.exe \"D:\\Videot\\OBS\\Recordings\\Alchemy glithcless.mp4\" 30:00 35:00\n");
+		System.out.println("Like this: FastVideoCutter.exe \"D:\\Videot\\OBS\\Recordings\\Video.mp4\" 30:00 35:00\n");
 		
-		System.out.println("Start time and end time are in seconds (neither is required). OutputName is also optional (defaults to \"out\").");
-		System.out.println("For absolute path put drive letter and slash first, and end it with file ext.");
-		System.out.println("End time can be duration too if you put d (for duration) right before the time.");
-		System.out.println("Like so: d5:00 (means 5 minutes of video).");
+		System.out.println("Or you can use flags. If you want to mix flags with the default format, put them last.");
+		
+		System.out.println("Start time and end time are optional (defaults to video start and end times respectively).");
 		System.out.println("Time format always includes seconds. You can add others intuitively.");
 		System.out.println("':' separates hours, minutes and seconds. '.' separates seconds from milliseconds.");
-		System.out.println("Doesn't override a file, so make sure the outputName doesn't already exist in the directory.\n");
+		System.out.println("OutputName is also optional (defaults to \"out\" and same type as input file).");
+		System.out.println("For absolute path put drive letter, colon and slash first, and end it with file ext.");
+		System.out.println("End time can be duration too if you put d (for duration) right before the time.");
+		System.out.println("Like so: d5:00 (means 5 minutes of video). (You can also use flags as program arguments)");
+		System.out.println("Doesn't override a file, so make sure the outputName doesn't already exist in the directory.");
+		System.out.println("You can change format by giving type extension at the end of the outputName.\n");
+		
+		System.out.println("Flags:");
+		System.out.println("-i or --input\t\tInput file location.");
+		System.out.println("-s or --start\t\tStart time.");
+		System.out.println("-e or --end\t\tEnd time.");
+		System.out.println("-d or --duration\tDuration (Use either this or End time).");
+		System.out.println("-o or --output\t\tOutput file name.");
+		System.out.println("-a or --accurate\tAccurate timings. Won't use I-frames, so is accurate but slow. Will re-encode the video.\n");
+		
+		System.out.println("Flag usage: FastVideoCutter.exe input.mp4 -s 15:35 -e 21:13 -o outputName.mp4");
+		System.out.println("If -o is last flag it can be omitted and have only outputName for convenience.\n");
 	}
 	
 	private static boolean isWindowsAbsolutePath(String path) {
